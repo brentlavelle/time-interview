6484223ed52c6dd3398867e296ca5901cb59bebd
brentlavelle <brentlavelle@yahoo.com>
2020-04-09 02:49:47 -0500

    Parsing and production of AM/PM added with tests while 24 hour format is unknown

--- lib/time_mod.rb
+++ lib/time_mod.rb
@@ -1,10 +1,67 @@
 #"[H]H:MM {AM|PM}", and the second argument is a signed integer indicating the number of minutes to add to the time given in the first argument.
 module TimeMod
+  def self.parse_to_minutes_12(time_string)
+    raise ArgumentError unless m = time_string.match(/^(1?\d)\:(\d\d) ([AP]M)$/)
+    if m[3] == 'AM'
+      if m[1] == '12'
+        hour = 0
+      else
+        hour = m[1].to_i
+      end
+    else
+      if m[1] == '12'
+        hour = m[1].to_i
+      else
+        hour = m[1].to_i + 12
+      end
+    end
+    hour * 60 + m[2].to_i
+  end
+
+  def self.parse_to_minutes_24(time_string)
+    # Handle one of the 2 versions of midnight first
+    return 0 if time_string == '24:00'
+
+    raise ArgumentError unless m = (time_string.match /^([12]?\d)\:(\d\d)$/)
+    m[1].to_i * 60 + m[2].to_i
+  end
+
+
+  def self.format_time_12(minutes)
+    ampm = 'AM'
+    minutes    = minutes % (60 * 24) # Removes the day offset
+    minute_pos = minutes % 60
+    hour_pos   = (minutes - minute_pos) / 60
+    if hour_pos == 0
+      hour = 12
+    elsif hour_pos < 12
+      hour = hour_pos
+    elsif hour_pos == 12
+      ampm = 'PM'
+      hour = 12
+    else
+      hour = hour_pos - 12
+      ampm = 'PM'
+    end
+    "#{hour}:#{'%02d' % minute_pos} #{ampm}"
+    "#{hour}:#{'%02d' % minute_pos} #{ampm}"
+  end
+
+  def self.format_time_24(minutes)
+    minutes    = minutes % (60 * 24) # Removes the day offset
+    minute_pos = minutes % 60
+    hour_pos   = (minutes - minute_pos) / 60
+  end
+
 
   def self.add(time_string, offset_minutes)
     raise ArgumentError unless offset_minutes.class == Integer
     raise ArgumentError unless time_string.class == String
-
-    time_string
+    if time_string.end_with?'M'
+      self.format_time_12(self.parse_to_minutes_12(time_string) + offset_minutes)
+    else
+      self.format_time_24(self.parse_to_minutes_24(time_string) + offset_minutes)
+    end
   end
+
 end
--- spec/test_time_spec.rb
+++ spec/test_time_spec.rb
@@ -2,18 +2,68 @@ require 'rspec'
 require_relative '../lib/time_mod'
 
 describe 'Parameters' do
-  context 'not an int' do
-    it 'works with a 0 offset' do
-      some_time = "12:55 PM"
-      expect(TimeMod::add(some_time, 0)).to eq(some_time)
+  context 'AM/PM tests' do
+    it 'works with a +1 offset' do
+      expect(TimeMod::add("12:55 PM", 1)).to eq("12:56 PM")
     end
 
+    it 'works with a -1 offset' do
+      expect(TimeMod::add("1:15 AM", -1)).to eq("1:14 AM")
+    end
+
+    it 'can cross the noon hour boundary in both directions' do
+      expect(TimeMod::add("11:05 AM", 62)).to eq("12:07 PM")
+      expect(TimeMod::add("12:07 PM", -62)).to eq("11:05 AM")
+    end
+
+    it 'midnight to midnight' do
+      expect(TimeMod::add("12:00 AM", 24*60)).to eq("12:00 AM")
+    end
+
+    it 'midnight to noon backwards' do
+      expect(TimeMod::add("12:00 AM", -12*60)).to eq("12:00 PM")
+    end
+
+    it 'can go a year and a minute into the future' do
+      expect(TimeMod::add("7:02 AM", 365*24*60+1)).to eq("7:03 AM")
+
+    end
+
+    it 'exhaustive set 0 offset for all times' do
+      # This is overkill but proves that all the inputs are parsed and produced
+      (1..12).each do |hour|
+        (0..0).each do |minute|
+          ['AM','PM'].each do |am_or_pm|
+            some_time = "#{hour}:#{'%02d' % minute} #{am_or_pm}"
+            expect(TimeMod::add(some_time, 0)).to eq(some_time)
+            end
+        end
+      end
+    end
+  end
+
+  context 'AM/PM negative tests' do
     it 'use a Float instead of an int' do
       expect { TimeMod::add("12:30 AM", 10.5) }.to raise_error(ArgumentError)
     end
 
     it 'needs the time to be a string' do
-      expect { TimeMod::add(15, 1) }.to raise_error(ArgumentError)
+      expect { TimeMod::add(1215, 1) }.to raise_error(ArgumentError)
+    end
+
+    it 'needs the time formatted correctly' do
+      [
+          '123:30 AM',
+          '9:130 AM',
+          '11:13 TM',
+          '11:1 AM',
+          '1201 AM',
+          '',
+          '∰',
+          '﷽',
+      ].each do |time|
+        expect { TimeMod::add(time, 2) }.to raise_error(ArgumentError)
+      end
     end
   end
 end
